9194999f56442e3c10c4d5a9916bf3ddb8d3dc2e
Karanjitsinghaxt <116353607+Karanjitsinghaxt@users.noreply.github.com>
2022-11-03 14:55:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import uuid
 import os
 print(os.environ['USER_NAME'])
+if(os.environ['USER_NAME']=='Karanjit'):
+  print('file is successfully encrypted')
+else:
+  print('file is uncessfully encrypted')
 x = uuid.uuid1()
 print(uuid.uuid5(x,"name"))
